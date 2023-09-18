b69f90ccdd7a19aee992213522b38d89eac8ee55
diojer <diogodinosaurus@gmail.com>
2023-09-18 13:06:46 +0100

    added env variable

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use("/sponsors", require("./routes/Sponsors"));
 app.use("/reports", require("./routes/Reports"));
 
 db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
+  app.listen(process.env.PORT || 3001, () => {
     console.log("Server running on Port 3001");
   });
 });
