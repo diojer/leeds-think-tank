d86d1faa3ce7d720d6b9ee0b6c4cf52adb300c1a
diojer <diogodinosaurus@gmail.com>
2023-09-19 22:22:44 +0100

    added new login and regist. functionality

--- routes/Users.js
+++ routes/Users.js
@@ -8,15 +8,60 @@ router.get("/", async (req, res) => {
   res.json(listOfUsers);
 });
 
+router.post("/login", async (req, res) => {
+  const { username, password, remember } = req.body;
+  const user = await Users.findOne({ where: { username: username } });
+  if (!user) {
+    res.json({
+      message: "Username incorrect",
+      valid: false,
+    });
+    return;
+  }
+  bcrypt.compare(password, user.password, (err, result) => {
+    if (err) {
+      res.json({
+        message: err,
+        valid: false,
+      });
+      return;
+    }
+    if (result) {
+      res.json({
+        message: "Login successful",
+        valid: true,
+      });
+    } else {
+      res.json({
+        message: "Password incorrect",
+        valid: false,
+      });
+    }
+  });
+});
+
 router.post("/", async (req, res) => {
   const { username, password, email } = req.body;
+  const existingUser = await Users.findOne({ where: { username: username } });
+  const existingEmail = await Users.findOne({ where: { email: email } });
+  if (existingUser) {
+    res.json({
+      message: "Username already exists",
+      valid: false,
+    });
+    return;
+  }
+  if (existingEmail) {
+    res.json({ message: "Email already in use", valid: false });
+    return;
+  }
   bcrypt.hash(password, 10).then((hash) => {
     Users.create({
       username: username,
       password: hash,
       email: email,
     });
-    res.json("User Added");
+    res.send({ message: "Registered Successfully", valid: true });
   });
 });
 
